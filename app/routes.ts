afe3af2544e03057094aae573c4f9f29b8770039
AnwarAsry <anwareducation03@gmail.com>
2026-03-05 22:11:09 +0100

    routes done

--- app/routes.ts
+++ app/routes.ts
@@ -4,22 +4,22 @@ export default [
     // Default route (home page)
     index("routes/home.tsx"),
 
-    // All Categories share the same layout
-    layout("routes/category/category-layout.tsx", [
-        route("electronics", "routes/category/electronics.tsx"),
-        route("clothing", "routes/category/clothing.tsx"),
-        route("home-living", "routes/category/home-living.tsx"),
-        route("garden", "routes/category/garden.tsx"),
-        route("books", "routes/category/books.tsx"),
-        route("sports", "routes/category/sports.tsx"),
-        route("beauty", "routes/category/beauty.tsx"),
+    // All Categories and search results share the same layout
+    layout("routes/Layouts/products-layout.tsx", [
+        route("electronics", "routes/Category/electronics.tsx"),
+        route("clothing", "routes/Category/clothing.tsx"),
+        route("home-living", "routes/Category/home-living.tsx"),
+        route("garden", "routes/Category/garden.tsx"),
+        route("books", "routes/Category/books.tsx"),
+        route("sports", "routes/Category/sports.tsx"),
+        route("beauty", "routes/Category/beauty.tsx"),
 
         // Search results page
         route("search", "routes/search.tsx"),
     ]),
 
     // Product details ex. /product/electronics/42
-    route("product/:category/:id", "routes/product-detail.tsx"),
+    route("product/:category/:id", "routes/Product/product-detail.tsx"),
 
     // Account related routes
     ...prefix("account", [
