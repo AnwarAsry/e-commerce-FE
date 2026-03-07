50d3ed12a2f2a975894e9a015dc7ab15d4b0757a
AnwarAsry <anwareducation03@gmail.com>
2026-03-07 15:07:17 +0100

    not implemented

--- app/actions/ProductsAction.ts
+++ app/actions/ProductsAction.ts
@@ -1,6 +1,10 @@
-import type { IProduct } from "~/model/Product";
+import { DUMMY_PRODUCTS, type IProduct } from "~/model/Product";
 import type { ServerActionResponse } from "~/model/responses/ServerActionResponse";
 
 export const getCategoryProducts = async (category: string, sort: string, inStock: boolean): Promise<ServerActionResponse<IProduct[] | null>> => {
     return { data: null, message: "Not implemented", success: false };
 };
+
+export const getProduct = async (id: string): Promise<ServerActionResponse<IProduct | null>> => {
+    return { data: null, message: "Not implemented", success: true };
+}
