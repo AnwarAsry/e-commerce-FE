b023047d979735316e7832839635b772ce01be5b
AnwarAsry <anwareducation03@gmail.com>
2026-03-08 19:05:12 +0100

    getCategoryProducts

--- app/actions/ProductsAction.ts
+++ app/actions/ProductsAction.ts
@@ -1,8 +1,19 @@
-import { DUMMY_PRODUCTS, type IProduct } from "~/model/Product";
+import { get } from "~/lib/ServiceBase";
+import { type IProduct } from "~/model/Product";
 import type { ServerActionResponse } from "~/model/responses/ServerActionResponse";
 
 export const getCategoryProducts = async (category: string, sort: string, inStock: boolean): Promise<ServerActionResponse<IProduct[] | null>> => {
-    return { data: null, message: "Not implemented", success: false };
+    try {
+        const response = await get<ServerActionResponse<IProduct[]>>(`/${category}?sort=${sort}&inStock=${sort}`);
+
+        if (!response.success) {
+            return { data: null, message: "Server Error", success: false }
+        }
+
+        return response;
+    } catch (error) {
+        return { data: null, message: JSON.stringify(error), success: false };
+    }
 };
 
 export const getProduct = async (id: string): Promise<ServerActionResponse<IProduct | null>> => {
