37e8f96a6d32d1ba50b37d36068958437c17ee1a
AnwarAsry <anwareducation03@gmail.com>
2026-03-08 16:42:04 +0100

    get featured only

--- app/actions/ProductsAction.ts
+++ app/actions/ProductsAction.ts
@@ -8,3 +8,7 @@ export const getCategoryProducts = async (category: string, sort: string, inStoc
 export const getProduct = async (id: string): Promise<ServerActionResponse<IProduct | null>> => {
     return { data: null, message: "Not implemented", success: true };
 }
+
+export const getFeaturedProducts = async (): Promise<ServerActionResponse<IProduct[] | null>> => {
+    return { data: null, message: "Not implemented", success: true };
+}
