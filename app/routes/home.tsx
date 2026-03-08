c4720d9a79d093bfd98fe861a7605765be121f78
AnwarAsry <anwareducation03@gmail.com>
2026-03-08 16:52:03 +0100

    added comp to home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,10 @@
 import { DisclaimerModal } from "~/components/DisclaimerModal";
 import type { Route } from "./+types/home";
 import { CategoryGrid } from "~/components/CategoryGrid";
+import { FeaturedProducts } from "~/components/FeaturedProducts";
+import type { IProduct } from "~/model/Product";
+import { useLoaderData } from "react-router";
+import { getFeaturedProducts } from "~/actions/ProductsAction";
 
 export function meta({ }: Route.MetaArgs) {
 	return [
@@ -9,9 +13,23 @@ export function meta({ }: Route.MetaArgs) {
 	];
 }
 
+interface HomeLoaderData {
+	featured: IProduct[];
+}
+
+export async function loader(): Promise<HomeLoaderData> {
+	const response = await getFeaturedProducts();
+	return {
+		featured: response.success && response.data ? response.data : [],
+	};
+}
+
 export default function Home() {
+	const { featured } = useLoaderData<HomeLoaderData>();
+
 	return <>
 		<DisclaimerModal />
 		<CategoryGrid />
+		<FeaturedProducts products={featured} />
 	</>;
 }
